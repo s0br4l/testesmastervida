8317202bb1223be090b48f1deddfadf69c70724f
lsobral <lucas.sobral5@gmail.com>
2022-07-21 04:08:40 -0300

    Added heroku configs

---
+++ configs/settings/dev.py
@@ -0,0 +1,14 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-j(7%1%kul)n84$i#(kzi-mk6)&q@1+-1xt(g7_w_15k1+h-xm='
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
---
+++ configs/settings/prod.py
@@ -0,0 +1,18 @@
+from .base import *
+import django_on_heroku
+from decouple import config
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'testesmastervida.herokuapp.com'
+]
+
+
